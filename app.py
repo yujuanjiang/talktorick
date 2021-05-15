58a9f0e7caf5da98399176f2dde057899afde143
Yujuan Jiang <yujuan.jiang@polymtl.ca>
2021-05-15 10:59:04 -0400

    Update Flask architecture

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+import pandas as pd
+import pickle
+import numpy as np
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+
+model = load_model('chat_model')
+
+# load tokenizer object
+with open( 'tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+# load label encoder object
+with open( 'label_encoder.pickle', 'rb') as enc:
+    lbl_encoder = pickle.load(enc)
+
+# parameters
+max_len = 20
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+
+        inp = request.form.values()
+
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+        tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+        for i in data['intents']:
+            if i['tag'] == tag:
+                return render_teplate('homt.html', pred = 'Rick says: '.format(i['responses']))
+
+
